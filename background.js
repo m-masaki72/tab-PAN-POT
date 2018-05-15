f8a487be00374a4e2ee09906fb0c7550f1ec3ce5
Masaki Mori <m-masaki@slp.nitech.ac.jp>
2018-05-15 18:47:25 +0900

    firstCommit

---
+++ background.js
@@ -0,0 +1,157 @@
+class myTabCapture{
+   constructor(tab){
+      chrome.browserAction.setBadgeText({text: "ON"});
+      this.init(tab);
+   }
+   destructor(){
+            chrome.browserAction.setBadgeText({text: ""});
+      this.clear();
+   }
+
+   init(tab){
+      this.clear();
+      this.tab = tab;
+      this.tabid = tab.id;
+      this.getStream();
+   }
+
+   clear(){
+      this.tab = null;
+      this.tabid = null;
+      this.serveilanve = null;
+      if(this.stream){
+         let track = this.stream.getTracks();
+         track.forEach((t) => {
+            t.stop();
+         });
+      }
+      this.stream = null;
+
+      if(this.source)
+      this.source.disconnect(this.panner);
+      this.source = null;
+      if(this.panner)
+      this.panner.disconnect(audioCtx.destination);
+      this.panner = null;
+      this.stream = null;
+   }
+
+   connectStream(){
+      this.source = audioCtx.createMediaStreamSource(this.stream);
+      this.panner = audioCtx.createStereoPanner();
+      this.source.connect(this.panner);
+      this.panner.connect(audioCtx.destination);
+   }
+
+   getStream() {
+      let that = this;
+      chrome.tabCapture.capture(
+         {video: false,  audio: true}, (stream) => {
+            if (!stream) {
+               console.error('Error starting tab capture: ' +
+               (chrome.runtime.lastError.message || 'UNKNOWN'));
+               return;
+            }
+            that.stream = stream;
+            that.connectStream();
+         }
+      );
+   }
+
+   setPanValue(val){
+      this.panner.pan.setValueAtTime(val, audioCtx.currentTime);
+   }
+
+   refreshIcon(){
+      chrome.browserAction.setBadgeText({text: "ON"});
+   }
+}
+
+class myTabArray{
+   constructor(){
+      this.init();
+   }
+   destructor(){
+      this.clear();
+   }
+
+   init(){
+      this.clear();
+      this.tabMap = new Map();
+   }
+
+   clear(){
+      if(this.tabMap)
+      this.tabMap.clear();
+      this.tabMap = null;
+   }
+
+   surveiStart(){
+      this.surveiStop();
+      this.surveillance = setInterval(() => {
+         chrome.windows.getAll({"populate" : true}, (chromeWindows) => {
+            chromeWindows.forEach((cw) =>{
+               let val = (cw.left + cw.width/2) / window.parent.screen.width - 1.0;
+               cw.tabs.forEach((tab) =>{
+                  if(this.tabMap.has(tab.id)){
+                     this.tabMap.get(tab.id).setPanValue(val);
+                  }
+
+               });
+            });
+         });
+      }, 200);
+   }
+
+   surveiStop(){
+      if(this.surveillance)
+      clearInterval(this.surveillance);
+   }
+
+   addTab(tab){
+      if(this.tabMap.has(tab.id)){
+         this.tabMap.get(tab.id).destructor();
+         this.tabMap.delete(tab.id);
+         if(this.tabMap.size <= 0)
+         this.surveiStop();
+      }else{
+         if(this.tabMap.size <= 0){
+            this.surveiStart();
+         }
+         this.tabMap.set(tab.id, new myTabCapture(tab));
+      }
+   }
+
+   deleteTabByID(tabID){
+      if(this.tabMap.has(tabID)){
+         this.tabMap.get(tabID).destructor();
+         this.tabMap.delete(tabID)
+         if(this.tabMap.size <= 0){
+            this.surveiStop();
+         }
+      }
+   }
+
+   refreshIconByID(tabID){
+      if(this.tabMap.has(tabID)){
+         this.tabMap.get(tabID).refreshIcon();
+      }
+   }
+}
+
+const audioCtx = new AudioContext();
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+const tabArray = new myTabArray;
+
+/* set event listners*/
+chrome.browserAction.onClicked.addListener((tab) => {
+   tabArray.addTab(tab);
+});
+
+chrome.tabs.onRemoved.addListener((tabID, windowInfo) => {
+   tabArray.deleteTabByID(tabID);
+});
+chrome.tabs.onSelectionChanged.addListener((tabID) => {
+      chrome.browserAction.setBadgeText({text: ""});
+   tabArray.refreshIconByID(tabID);
+});
